6ebe3970d7aa96f142e6ee76ffe0583f0207f64c
Zarni Hlawn <zarnihlawn@outlook.com>
2025-05-31 14:33:19 +0630

    Init

---
+++ src/main/java/software/mari/flow_server/FlowServerApplication.java
@@ -0,0 +1,13 @@
+package software.mari.flow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/software/mari/flow_server/FlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package software.mari.flow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
